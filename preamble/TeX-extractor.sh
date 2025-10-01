36a1573a52e78fc178d88ddfb04553aacefdd186
GrassGlass <108970334+GrassGlass@users.noreply.github.com>
2025-10-01 06:04:42 +0000

    - Separated `\Input` and `\KeywordForInput` into two distinct `.tex` files.

--- preamble/TeX-extractor.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-while IFS= read -r LINE; do
-    preamble.tex=$(printf '%s' "$LINE" | sed -n 's/^\\Input{\(.*\)}$/\1/p')
-    [ -z "$FILE" ] &&
-        printf '%s\n' "$LINE" ||
-        cat -- "$(kpsewhich "$FILE")"
-done <"$1"
